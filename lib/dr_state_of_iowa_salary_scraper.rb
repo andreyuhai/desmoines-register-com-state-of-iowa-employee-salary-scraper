0f2e5bf2af71c1b662117262994374d622a9d3a2
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-07-18 23:44:16 +0200

    Rescuing mysql duplicate error and added the function to scrape between page numbers

--- lib/dr_state_of_iowa_salary_scraper.rb
+++ lib/dr_state_of_iowa_salary_scraper.rb
@@ -18,17 +18,17 @@ class DRStateOfIowaSalaryScraper
     @agent = Mechanize.new
   end
 
-  def scrape
-    response = @agent.get 'https://db.desmoinesregister.com/state-salaries-for-iowa'
-    parsed_response_body = Nokogiri::HTML(response.body)
-
-    page_count = get_page_count(parsed_response_body)
-
-    (1..page_count).each do |page_num|
-      puts "[#{page_num}]\t--------------------------"
+  def scrape(from_page = 1, to_page = number_of_pages)
+    puts "Scraping through pages #{from_page} - #{to_page}"
+    (from_page..to_page).each do |page_num|
+      puts "[#{page_num}]\t----------------------------------------------------------"
       url = "https://db.desmoinesregister.com/state-salaries-for-iowa/page=#{page_num}&ordercol=col11&orderdir=desc"
 
-      response = @agent.get url
+      begin
+        response = @agent.get url
+      rescue Net::ReadTimeout, Mechanize::ResponseCodeError
+        retry
+      end
       parsed_response_body = Nokogiri::HTML(response.body)
 
       rows = parsed_response_body.xpath("//table[@class='responsive full']/tbody/tr")
@@ -47,7 +47,6 @@ class DRStateOfIowaSalaryScraper
         fy_salary = cells[7].text.match(/\d*,*\d+\.*\d*/).to_s.gsub(',', '').to_f
         fy = cells[8].text.to_i
 
-        puts "\t\t[#{index}]\t#{employee_name}\t#{department}\t#{july_salary}\t#{travel}\t#{fy_salary}\t#{fy}"
         params = {
           table_name: @table_name,
           query: {
@@ -63,12 +62,25 @@ class DRStateOfIowaSalaryScraper
             created_by: @scrape_dev
           }
         }
-        @db.insert_into_table(params)
+        begin
+          @db.insert_into_table(params)
+          puts "\t\t[#{index.to_s.ljust(2)}\t#{employee_name.ljust(20)}\t#{department.ljust(20)}\t#{july_salary.to_s.ljust(10)}\t#{travel.to_s.ljust(5)}\t#{fy_salary.to_s.ljust(10)}\t#{fy.to_s.ljust(10)}"
+        rescue Mysql2::Error => e
+          if e.error_number.eql? 1062
+            puts "\t\t[#{index.to_s.ljust(2)}\t#{employee_name.ljust(20)}\t#{department.ljust(20)}\t#{july_salary.to_s.ljust(10)}\t#{travel.to_s.ljust(5)}\t#{fy_salary.to_s.ljust(10)}\t#{fy.to_s.ljust(10)}\tDUPLICATE"
+          end
+        end
       end
     end
   end
 
-  def get_page_count(parsed_response_body)
+  def scrape_between(from_page, to_page)
+    scrape(from_page, to_page)
+  end
+
+  def number_of_pages
+    response = @agent.get 'https://db.desmoinesregister.com/state-salaries-for-iowa'
+    parsed_response_body = Nokogiri::HTML(response.body)
     parsed_response_body.xpath("(//ul[@class='pagination'])[1]/li[not(@class='arrow')][last()]").first.text.to_i
   end
 
